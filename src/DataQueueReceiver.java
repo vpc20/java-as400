7ef8d4596b0a15b8fdbf272b7961c474a412629a
vpc20 <cuevasvp@gmail.com>
2026-03-07 15:50:12 +0800

    revision

--- src/DataQueueReceiver.java
+++ src/DataQueueReceiver.java
@@ -7,7 +7,7 @@ public class DataQueueReceiver {
     public static void main(String[] args) throws Exception {
 
         // 1. Connect to IBM i
-        AS400 system = new AS400("PUB400.COM", "VPCRZKH", "vpcrzkh41");
+        AS400 system = new AS400("PUB400.COM", "user", "password");
 
         // Force connection immediately so you know it's connected
         system.connectService(AS400.DATAQUEUE);
